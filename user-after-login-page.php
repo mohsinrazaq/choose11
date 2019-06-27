44bb76be8f4509dc75099502510fed21912fc2b3
Mohsin Razzaq <mohsinrazzaq25@gmail.com>
2019-06-27 12:42:06 +0500

    working on user after login page

--- user-after-login-page.php
+++ user-after-login-page.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/navbarstyles.css">
+    <link rel="stylesheet" href="css/user-page.css">
+    <link rel="stylesheet" href="css/footer.css">
     <title>CHOOSE11 - User Page</title>
 </head>
 <body>
@@ -17,13 +19,30 @@
                 <ul>
                     <li><a href="#" class="current">Home</a></li>
                     <li><a href="#">About</a></li>
-                    <li><a href="#">settings</a></li>
+                    <li><a href="#">Settings</a></li>
                     <li><a href="#">Contact Us</a></li>
                     <li><a href="logout.php">Logout</a></li>       
                 </ul>
             </nav>
         </header>
         <!-- header ended here -->
+
+        <div class="inner-wrapper">
+            <div class="left">
+
+            </div>
+            <div class="center">
+
+            </div>
+            <div class="right">
+
+            </div>
+        </div>
+        <!-- footer started -->
+            <footer>
+            <h3 class="footer">&copy;2019 choose11. All rights reserved</h3> 
+            </footer>
+       <!-- footer ended -->
     </div>
 </body>
 </html>
