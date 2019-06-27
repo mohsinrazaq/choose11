025401ecc7bee06d44431f3fb33be217500505bf
Mohsin Razzaq <mohsinrazzaq25@gmail.com>
2019-06-27 14:32:39 +0500

    working on uer after login page

--- user-after-login-page.php
+++ user-after-login-page.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/navbarstyles.css">
     <link rel="stylesheet" href="css/user-page.css">
+    <link rel="stylesheet" href="css/footer.css">
     <title>CHOOSE11 - User Page</title>
 </head>
 <body>
@@ -13,7 +14,7 @@
         <!-- header started here -->
         <header>
             <a href="user-after-login-page.php" id="logo"></a>
-            <nav>
+            <!-- <nav>
                 <a href="#" id="menu-icon"></a>
                 <ul>
                     <li><a href="#" class="current">Home</a></li>
@@ -22,22 +23,38 @@
                     <li><a href="#">Contact Us</a></li>
                     <li><a href="logout.php">Logout</a></li>       
                 </ul>
-            </nav>
+            </nav> -->
         </header>
         <!-- header ended here -->
 
         <div class="inner-wrapper">
             <div class="left">
-                <div class="inner-left">
-
-                </div>
+              <div class="inner-left-top">
+              <nav>
+                <a href="#" id="menu-icon"></a>
+                <ul>
+                    <li><a href="#" class="current">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Settings</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="logout.php">Logout</a></li>       
+                </ul>
+            </nav>  
+              </div>
             </div>
             <div class="right">
-
+                <div class="inner-right">
+                    
+                </div>
 
             </div>
-        </div>
-    
+        </div> 
+
+        <!-- footer started -->
+       <footer>
+          <h3 class="footer">&copy;2019 choose11. All rights reserved</h3> 
+       </footer>
+       <!-- footer ended -->  
     </div>
 </body>
 </html>
