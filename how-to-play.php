ebf2cc5e248100606320a89ec21cd49fd0b3a0ba
Mohsin Razzaq <mohsinrazzaq25@gmail.com>
2019-06-28 05:04:49 +0500

    working on menu

--- how-to-play.php
+++ how-to-play.php
@@ -5,19 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/how-to-pay-style.css">
-    <title>Document</title>
+    <title>How To Play </title>
 </head>
 <body>
     <div class="wrapper">
         <div class="left">
             <div class="left-top">
-                
+                    <p>How To Play</p>
+                    
+        <div class="collapsible-menu">
+            <button class="collapsible">Open Section 1</button>
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <button class="collapsible">Open Section 2</button>
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+            <button class="collapsible">Open Section 3</button>
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </div>
+
             </div>
         </div>
         <div class="right">
 
         </div>
     </div>
-    <script src="js/how-to-play-script.js"></script>
+    <script src="js/how-to-paly-script.js"></script>
 </body>
 </html>
